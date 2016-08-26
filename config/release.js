8d572c2f14413bb94da2f09cf4cd700ff79a7fb4
Chris Ball <chris@echobind.com>
2016-08-26 09:14:54 -0400

    disable commit hook

--- config/release.js
+++ config/release.js
@@ -3,10 +3,10 @@
 var execSync = require('child_process').execSync;
 
 module.exports = {
-  beforeCommit: function(project, versions) {
-    // generate changelog
-    runCommand('github_changelog_generator --simple-list --future-release='+versions.next);
-  }
+  // beforeCommit: function(project, versions) {
+  //   // generate changelog
+  //   runCommand('github_changelog_generator --simple-list --future-release='+versions.next);
+  // }
 };
 
 function runCommand(command) {
