9d80729961455025b964612c44bc417bd69988a3
Chris Ball <chris@echobind.com>
2016-08-26 09:13:40 -0400

    finish examples, add release.js

---
+++ app/services/shared-something.js
@@ -0,0 +1,1 @@
+export { default } from 'moment-addon-example/services/shared-something';
---
+++ config/release.js
@@ -0,0 +1,16 @@
+/* jshint node:true */
+/* var RSVP = require('rsvp'); */
+var execSync = require('child_process').execSync;
+
+module.exports = {
+  beforeCommit: function(project, versions) {
+    // generate changelog
+    runCommand('github_changelog_generator --simple-list --future-release='+versions.next);
+  }
+};
+
+function runCommand(command) {
+  console.log('running: ' + command);
+  var output = execSync(command, { encoding: 'utf8' });
+  console.log(output);
+}
